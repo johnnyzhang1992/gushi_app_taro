9951bed95c41e283b44a8fbd00fb515d6fd861bf
johnnyzhang1992 <zq19920306@gmail.com>
2021-04-27 22:26:11 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gushi_app_taro
+
 古诗文小程序taro版本
--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,58 @@
-import { Component } from 'react'
-import { Provider } from 'react-redux'
+import { Component } from 'react';
+import { Provider } from 'react-redux';
+import Taro from '@tarojs/taro';
 
-import configStore from './store'
+import configStore from './store';
 
-import './app.scss'
+import './app.scss';
 
-const store = configStore()
+const store = configStore();
 
 class App extends Component {
-  componentDidMount () {}
+	componentDidMount() {
+		// console.log(Taro.ENV_TYPE);
+		// 应用载入时，传入的相关参数
+		const AppOptions = Taro.getLaunchOptionsSync();
+		console.log(AppOptions);
+		const ENV_TYPE = Taro.getEnv();
+		// 微信小程序
+		if (ENV_TYPE === 'WEAPP') {
+			// 更新小程序逻辑
+			const updateManager = Taro.getUpdateManager();
+			updateManager.onCheckForUpdate(function (res) {
+				// 请求完新版本信息的回调
+				console.log('小程序是否有更新：'+res.hasUpdate);
+			});
+			updateManager.onUpdateReady(function () {
+				Taro.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function (res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate();
+						}
+					},
+				});
+			});
+			updateManager.onUpdateFailed(function () {
+				// 新的版本下载失败
+				console.log("更新失败")
+			});
+		}
+	}
 
-  componentDidShow () {}
+	componentDidShow() {}
 
-  componentDidHide () {}
+	componentDidHide() {}
 
-  componentDidCatchError () {}
+	componentDidCatchError() {}
 
-  // 在 App 类中的 render() 函数没有实际作用
-  // 请勿修改此函数
-  render () {
-    return (
-      <Provider store={store}>
-        {this.props.children}
-      </Provider>
-    )
-  }
+	// 在 App 类中的 render() 函数没有实际作用
+	// 请勿修改此函数
+	render() {
+		return <Provider store={store}>{this.props.children}</Provider>;
+	}
 }
 
-export default App
+export default App;

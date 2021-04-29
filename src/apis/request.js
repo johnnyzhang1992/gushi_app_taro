3c212506ad142df49586afa3cf996e9a1e7df0c7
zq19920306 <1113638674@qq.com>
2021-04-29 15:53:51 +0800

    update add request.js file

---
+++ src/apis/request.js
@@ -0,0 +1,283 @@
+/**
+ * API 统一请求文件
+ * author: johnnyzhang
+ * github: https://github.com/johnnyzhang1992/gushi_lite
+ */
+import Taro from '@tarojs/taro';
+
+ const app = Taro.getApp();
+ const BaseUrl = app.globalData.url;
+
+ // 封装小程序远程请求函数
+ function Request(url, params = {}, method) {
+     let data = {
+         ...params,
+         openId: Taro.getStorageSync("user").openId,
+         wx_token: Taro.getStorageSync("wx_token")
+     };
+     return new Promise((resolve, reject) => {
+         //结果以Promise形式返回
+         Taro.request({
+             url: BaseUrl + url,
+             data: data,
+             type: method || "GET",
+             success: res => {
+                 if (res.data) {
+                     resolve(Object.assign(res, { succeeded: true }));
+                     //成功失败都resolve，并通过succeeded字段区分
+                 } else {
+                     reject(Object.assign(res, { succeeded: false }));
+                     //成功失败都resolve，并通过succeeded字段区分
+                 }
+             },
+             fail: error => {
+                 console.log(error);
+                 reject(Object.assign(error, { succeeded: false }));
+                 //成功失败都resolve，并通过succeeded字段区分
+             }
+         });
+     });
+ }
+
+ // 加载失败
+ export const LOADFAIL = msg => {
+     Taro.showToast({
+         title: msg || "加载失败",
+         icon: "none",
+         duration: 2000
+     });
+ };
+
+ // 返回登录前页面
+ export const BACKURL = () => {
+     if (app.globalData.backUrl && app.globalData.backUrl.url) {
+         // 问询是否返回登录前页面
+         Taro.showModal({
+             title: "登录成功",
+             content: "您是否想返回登录前的页面？",
+             cancelText: "不需要",
+             confirmText: "马上返回",
+             success: res => {
+                 console.log(res);
+                 // that.getUserDetail();
+                 let backUrl = app.globalData.backUrl;
+                 if (res && res.confirm) {
+                     // 现在去登录
+                     if (backUrl && backUrl.type && backUrl.type == "tab") {
+                         Taro.switchTab({
+                             url: backUrl.url
+                         });
+                     } else {
+                         Taro.navigateTo({
+                             url: backUrl.url
+                         });
+                     }
+                 }
+             },
+             fail: error => {
+                 console.log(error);
+                 Taro.showToast({
+                     title: "好像哪里出错了，请重试。",
+                     icon: "none",
+                     mask: true
+                 });
+             }
+         });
+     }
+ };
+
+ /**
+  * 创建新用户
+  * @param {*} method
+  * @param {*} data
+  */
+ export const CREATE_USER = (method, data) => {
+     return Request("/wxxcx/userCrate", data, method);
+ };
+
+ /**
+  * 获取小程序码
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_WX_QRCODE = (method, data) => {
+     return Request(`/wxxcx/getWXACode/`, data, method);
+ };
+
+ /**
+  * 获取用户的基本信息
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_USER_INFO = (method, data) => {
+     return Request(`/wxxcx/getUserInfo/${data.user_id}`, data, method);
+ };
+
+ /**
+  * 获取搜索列表
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_SEARCH_LIST = (method, data) => {
+     return Request(`/wxxcx/search_list`, data, method);
+ }
+
+ /**
+  * 更新搜索状态（删除数据库搜索记录）
+  * @param {*} method
+  * @param {*} data
+  */
+ export const UPDATE_SEARCH_STATE = (method, data) => {
+     return Request(`/wxxcx/search/${data.id}/update`, data, method);
+ }
+
+ //-----------------
+ //------ 首页 --
+ //-----------------
+
+ /**
+  * 获取一条随机名句
+  * @param {String} method
+  * @param {Object} data
+  */
+ export const GET_RANDOM_SENTENCE = (method, data) => {
+     return Request(`/wxxcx/getRandomSentence`, data, method);
+ };
+
+ /**
+  * 获取首页列表数据
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_HOME_DATA = (method, data) => {
+     return Request(`/wxxcx/getHomeData`, data, method);
+ };
+
+ //-----------------
+ //------ 个人中心 --
+ //-----------------
+
+ /**
+  * 获取我的收藏信息(诗词、名句、诗人)
+  * @param {String} method
+  * @param {Object} data
+  */
+ export const GET_USER_COLLECT = (method, data) => {
+     return Request(`/wxxcx/getCollect/${data.user_id}`, data, method);
+ };
+
+ /**
+  * 更新收藏状态
+  * @param {String} method
+  * @param {Object} data
+  * @param {poem,sentence,author} type
+  */
+ export const UPDATE_USER_COLLECT = (method, data) => {
+     return Request(`/wxxcx/updateCollect/${data.type}`, data, method);
+ };
+
+ //-----------------
+ //------ 搜索部分 --
+ //-----------------
+
+ /**
+  * 获取搜索热词
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_HOT_SEARCH = (method, data) => {
+     return Request(`/wxxcx/getsHotSearch`, data, method);
+ };
+
+ /**
+  * 根据关键字搜索
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_SEARCH = (method, data) => {
+     return Request(`/wxxcx/search/${data.key}`, data, method);
+ };
+
+ //-----------------
+ //------ 诗词 --
+ //-----------------
+
+ /**
+  * 根据条件获取诗词列表
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POEM_DATA = (method, data) => {
+     return Request(`/wxxcx/getPoemData`, data, method);
+ };
+
+ /**
+  * 获取诗词详情
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POEM_DETAIL = (method, data) => {
+     return Request(`/wxxcx/poem/${data.id}`, data, method);
+ };
+
+ /**
+  * 获取诗词内容
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POEM_CONTENT = (method, data) => {
+     return Request(`/wxxcx/getPoemContent/${data.id}`, data, method);
+ };
+
+ /**
+  * 获取诗词的语音
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POEM_AUDIO = (method, data) => {
+     return Request(`/wxxcx/getPoemAudio/${data.id}`, data, method);
+ };
+
+ //-----------------
+ //------ 诗人 --
+ //-----------------
+
+ /**
+  * 根据条件获取诗人列表
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POET_DATA = (method, data) => {
+     return Request(`/wxxcx/getPoetData?`, data, method);
+ };
+
+ /**
+  * 诗人详情
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_POET_DETAIL = (method, data) => {
+     return Request(`/wxxcx/getPoetDetailData/${data.id}`, data, method);
+ };
+
+ //-----------------
+ //------ 名句 --
+ //-----------------
+
+ /**
+  * 根据条件获取名句的列表
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_SENTEMCE_DATA = (method, data) => {
+     return Request(`/wxxcx/getSentenceData`, data, method);
+ };
+
+ /**
+  * 获取名句详情
+  * @param {*} method
+  * @param {*} data
+  */
+ export const GET_SENTEMCE_DETAIL = (method, data) => {
+     return Request(`/wxxcx/getSentenceDetail/${data.id}`, data, method);
+ };
--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,44 @@
-import { Component } from 'react';
-import { Provider } from 'react-redux';
-import Taro from '@tarojs/taro';
+import { Component } from "react";
+import { Provider } from "react-redux";
+import Taro from "@tarojs/taro";
 
-import configStore from './store';
+import configStore from "./store";
 
-import './app.scss';
+import "./app.scss";
 
 const store = configStore();
 
 class App extends Component {
 	componentDidMount() {
+		taroGlobalData = {
+			globalData: {
+				userInfo: null,
+				code: null,
+				systemInfo: null,
+				user: null,
+				hot: null,
+				domain: "https://xuegushi.cn/wxxcx",
+				url: "https://xuegushi.cn",
+				backUrl: {},
+			},
+		};
 		// console.log(Taro.ENV_TYPE);
 		// 应用载入时，传入的相关参数
 		const AppOptions = Taro.getLaunchOptionsSync();
 		console.log(AppOptions);
 		const ENV_TYPE = Taro.getEnv();
 		// 微信小程序
-		if (ENV_TYPE === 'WEAPP') {
+		if (ENV_TYPE === "WEAPP") {
 			// 更新小程序逻辑
 			const updateManager = Taro.getUpdateManager();
 			updateManager.onCheckForUpdate(function (res) {
 				// 请求完新版本信息的回调
-				console.log('小程序是否有更新：'+res.hasUpdate);
+				console.log("小程序是否有更新：" + res.hasUpdate);
 			});
 			updateManager.onUpdateReady(function () {
 				Taro.showModal({
-					title: '更新提示',
-					content: '新版本已经准备好，是否重启应用？',
+					title: "更新提示",
+					content: "新版本已经准备好，是否重启应用？",
 					success: function (res) {
 						if (res.confirm) {
 							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
@@ -37,7 +49,7 @@ class App extends Component {
 			});
 			updateManager.onUpdateFailed(function () {
 				// 新的版本下载失败
-				console.log("更新失败")
+				console.log("更新失败");
 			});
 		}
 	}

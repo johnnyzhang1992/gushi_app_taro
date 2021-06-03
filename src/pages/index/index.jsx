c66a5932935165a2d3459dc39f668fc48020482a
johnnyzhang1992 <zq19920306@gmail.com>
2021-06-03 22:08:07 +0800

    update 首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-// import { connect } from 'react-redux';
 import { View, Image, Navigator } from '@tarojs/components';
 // import { AtButton } from 'taro-ui';
 import Taro from '@tarojs/taro';
@@ -7,30 +6,14 @@ import Utils from '../../utils/util';
 
 import { GET_HOME_DATA, GET_RANDOM_SENTENCE } from '../../apis/request';
 
-// import { add, minus, asyncAdd } from '../../actions/counter';/
-
 import './index.scss';
 
 import boatPng from '../../assets/images/boat.png';
-
+import sentenceIcon from '../../assets/images/icon/sentence.png';
+import poemIcon from '../../assets/images/icon/poem.png';
+import poetIcon from '../../assets/images/icon/poet.png';
+import searchIcon from '../../assets/images/icon/find_active.png';
 const app = Taro.getApp();
-let homeInterval = null;
-// @connect(
-// 	({ counter }) => ({
-// 		counter,
-// 	}),
-// 	(dispatch) => ({
-// 		add() {
-// 			dispatch(add());
-// 		},
-// 		dec() {
-// 			dispatch(minus());
-// 		},
-// 		asyncAdd() {
-// 			dispatch(asyncAdd());
-// 		},
-// 	}),
-// )
 class Index extends Component {
 	state = {
 		date: Utils.formatDateToMb(),
@@ -39,6 +22,7 @@ class Index extends Component {
 	};
 
 	componentDidMount() {
+		// 获取每日一首诗词
 		GET_RANDOM_SENTENCE().then((res) => {
 			console.log(res.data);
 			if (res && res.statusCode === 200) {
@@ -48,6 +32,7 @@ class Index extends Component {
 				});
 			}
 		});
+		// 获取首页诗词列表数据
 		GET_HOME_DATA().then((res) => {
 			console.log(res.data);
 			if (res && res.statusCode === 200) {
@@ -65,13 +50,18 @@ class Index extends Component {
 
 	componentDidShow() {}
 
-	componentDidHide() {}
+	componentDidHide() { }
+	
+	onPullDownRefresh() {
+		Taro.stopPullDownRefresh();
+	}
 
 	render() {
 		const { date, hot } = this.state;
 
 		return (
 			<View className="container home-content">
+				{/* 首页每日诗词部分 */}
 				<View className="home-header">
 					<View className="h-content">
 						<View className="h-time y">{date[0]}</View>
@@ -87,6 +77,43 @@ class Index extends Component {
 					</View>
 					<Image className="h-boat boat-img" src={boatPng}></Image>
 				</View>
+				{/* 中间导航部分 */}
+				<View className="nav-list">
+					<Navigator
+						url="poem/index"
+						hover-class="Navigator-hover"
+						className="weui-flex__item nav-item"
+					>
+						<Image classNmae="" src={poemIcon} />
+						<View className="nav-item-text">诗词文言</View>
+					</Navigator>
+					<Navigator
+						url="sentence/index"
+						hover-class="Navigator-hover"
+						className="weui-flex__item nav-item"
+					>
+						<Image className="" src={sentenceIcon} />
+						<View className="nav-item-text">热门名句</View>
+					</Navigator>
+					<Navigator
+						url="poet/index"
+						hover-class="Navigator-hover"
+						className="weui-flex__item nav-item"
+					>
+						<Image className="" src={poetIcon} />
+						<View className="nav-item-text">历朝诗人</View>
+					</Navigator>
+					<Navigator
+						url="/pages/search/index"
+						open-type="switchTab"
+						hover-class="Navigator-hover"
+						className="weui-flex__item nav-item"
+					>
+						<Image className="" src={searchIcon} />
+						<View className="nav-item-text">搜索</View>
+					</Navigator>
+				</View>
+				<View className="divide"></View>
 			</View>
 		);
 	}
